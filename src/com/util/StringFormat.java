b238e67b0e69a3877b210aa7a9f73f108d2fd351
pnoker <peter-no@foxmail.com>
2016-09-29 19:05:32 +0800

    全角、半角相互转换

---
+++ src/com/util/StringFormat.java
@@ -0,0 +1,42 @@
+package com.util;
+
+public class StringFormat {
+	/**
+	 * 全角转半角
+	 * 
+	 * 全角格式：ｈｅｌｌｏｗ ｗｏｒｌｄ -> 半角格式：hellow world
+	 */
+	public String ToDBC(String input) {
+
+		char c[] = input.toCharArray();
+		for (int i = 0; i < c.length; i++) {
+			if (c[i] == '\u3000') {
+				c[i] = ' ';
+			} else if (c[i] > '\uFF00' && c[i] < '\uFF5F') {
+				c[i] = (char) (c[i] - 65248);
+
+			}
+		}
+		String returnString = new String(c);
+
+		return returnString;
+	}
+
+	/**
+	 * 半角转全角
+	 * 
+	 * 半角格式：hellow world -> 全角格式：ｈｅｌｌｏｗ ｗｏｒｌｄ
+	 */
+	public String ToSBC(String input) {
+		char c[] = input.toCharArray();
+		for (int i = 0; i < c.length; i++) {
+			if (c[i] == ' ') {
+				c[i] = '\u3000';
+			} else if (c[i] < '\177') {
+				c[i] = (char) (c[i] + 65248);
+
+			}
+		}
+		return new String(c);
+	}
+}
